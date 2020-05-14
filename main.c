535f00b5810bea9a1093e7463604d3769d78d2b4
Myduzo <sahli.youssef@outlook.com>
2020-05-14 17:37:14 +0100

    monty

--- main.c
+++ main.c
@@ -3,19 +3,15 @@
  * main - main program
  * @argc : number of arguments
  * @argv : argument command
- * Return: list
+ * Return: data
  */
 int main(int argc, char **argv)
 {
-	stack_t *stack = NULL;
 
 	if (argc != 2)
 		fprintf(stderr, "USAGE: monty file\n"), exit(EXIT_FAILURE);
-
-	file = fopen(argv[1], "r");
 	
-	if (!file)
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]), exit(EXIT_FAILURE);
+	read_file(argv[1]);
 
 	fclose(file);
 	return (0);
