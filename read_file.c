2ea9a9b5a79f62d597ea725d87b4cfa4eaaafdd3
Myduzo <sahli.youssef@outlook.com>
2020-05-15 19:07:33 +0100

    getline

--- read_file.c
+++ read_file.c
@@ -2,41 +2,34 @@
 char *data;
 /**
  * read_file - read a text file and print it as stdout
- * @file_name : name of the file 
+ * @file_name : name of the file
  */
 void read_file(const char *file_name)
 {
-    FILE *file;
-    char *str = NULL, *token1 = NULL, *token2 = NULL, *delim = "\n ";
-    size_t len = 0;
-    unsigned int l = 1;
-    int get_line;
-    stack_t *head = NULL;
-    void(*op)(stack_t **, unsigned int);
-    
-    file = fopen(file_name, "r");
-	
-    if (!file)
-    {
-		fprintf(stderr, "Error: Can't open file %s\n", file_name),
-        exit(EXIT_FAILURE);
-    }
-    while ((get_line = getline(&str, &len, file)) != -1)
-    {
-        token1 = strtok(str, delim);
-        token2 = strtok(NULL, delim);
-        data = token2;
+	FILE *file;
+	char *str = NULL, *token1 = NULL, *token2 = NULL, *delim = "\n ";
+	size_t len = 0;
+	unsigned int l = 1;
+	int get_line;
+	stack_t *head = NULL;
+	void (*op)(stack_t **, unsigned int);
 
-        op = op_code(token1);
-        if (!op)
-        {
-            fprintf(stderr,"L%d: unknown instruction %s", l, token1),
-            exit(EXIT_FAILURE);
-        }
+	file = fopen(file_name, "r");
 
-        op(&head, l);
-        l++;
-    }
-    fclose(file);
-    _free(head);
+	for (; (get_line = getline(&str, &len, file)) != -1; l++)
+	{
+		token1 = strtok(str, delim);
+		token2 = strtok(NULL, delim);
+		data = token2;
+
+		op = op_code(token1);
+		if (!op)
+		{
+			fprintf(stderr, "L%d: unknown instruction %s", l, token1);
+			exit(EXIT_FAILURE);
+		}
+
+		op(&head, l);
+	}
+	fclose(file), free(str), _free(head);
 }
