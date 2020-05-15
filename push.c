0618960467b76a11048b1e07e3a25f7204fe4c04
Myduzo <sahli.youssef@outlook.com>
2020-05-15 17:13:27 +0100

    push_func

--- push.c
+++ push.c
@@ -1,41 +1,38 @@
 #include "monty.h"
 /**
- * push - push and element to the stack
+ * _push - push and element to the stack
  * @stack : pointer to an address
  * @line_number : number of the line printed
  */
-void push(stack_t **stack, unsigned int line_number)
+void _push(stack_t **head, unsigned int line_number)
 {
     UNUSED(line_number);
-    stack_t *head;
     
-    head = malloc(sizeof(stack_t));
-    if (!head)
+    stack_t *stack;
+    stack = malloc(sizeof(stack_t));
+    if (!stack)
     {
-        fprintf(stderr, "Error: malloc failed\n"); 
-        fclose(file);
+        fprintf(stderr, "Error: malloc failed\n"),
+        _free(*head);
         exit(EXIT_FAILURE);
     }
 
-    if (!data || !isdigit(data))
+    if (!data || _isdigit(data) == 1)
     {
-        fprintf(stderr,"L%d: usage: push integer\n", line_number);
-        fclose(file);
+        fprintf(stderr,"L%d: usage: push integer\n", line_number),
         exit(EXIT_FAILURE);
     }
 
-    head->n = atoi(data);
-    head->prev = NULL;
+    stack->n = atoi(data);
+    stack->prev = NULL;
 
-    if(*stack)
+    if(*head)
     {
-        head->next = *stack;
-        (*stack)->prev = head;
-        *stack = head;
+        stack->next = *head;
+        (*head)->prev = stack;
     }
     else
-    {
-        head->next = NULL;
-        *stack = head;
-    }   
+        stack->next = NULL;
+
+    *head = stack;
 }
