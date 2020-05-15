7e798eaa29217479f8d89114b93a42fa34826081
Myduzo <sahli.youssef@outlook.com>
2020-05-15 17:12:21 +0100

    getline

--- read_file.c
+++ read_file.c
@@ -1,35 +1,42 @@
 #include "monty.h"
+char *data;
 /**
  * read_file - read a text file and print it as stdout
  * @file_name : name of the file 
  */
 void read_file(const char *file_name)
 {
+    FILE *file;
     char *str = NULL, *token1 = NULL, *token2 = NULL, *delim = "\n ";
     size_t len = 0;
-    unsigned int line_number = 1;
+    unsigned int l = 1;
     int get_line;
-    stack_t *stack;
+    stack_t *head = NULL;
     void(*op)(stack_t **, unsigned int);
     
     file = fopen(file_name, "r");
-    get_line = getline(&str, &len, file);
 	
     if (!file)
     {
-		fprintf(stderr, "Error: Can't open file %s\n", file_name);
+		fprintf(stderr, "Error: Can't open file %s\n", file_name),
         exit(EXIT_FAILURE);
     }
-    while (get_line != -1)
+    while ((get_line = getline(&str, &len, file)) != -1)
     {
         token1 = strtok(str, delim);
         token2 = strtok(NULL, delim);
         data = token2;
 
         op = op_code(token1);
+        if (!op)
+        {
+            fprintf(stderr,"L%d: unknown instruction %s", l, token1),
+            exit(EXIT_FAILURE);
+        }
 
-        op(&stack, line_number);
-        line_number++;
+        op(&head, l);
+        l++;
     }
     fclose(file);
+    _free(head);
 }
