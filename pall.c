ae3c89ff872b0de24e572f8f1af0cc23618bd372
Myduzo <sahli.youssef@outlook.com>
2020-05-15 17:13:57 +0100

    pall_func

--- pall.c
+++ pall.c
@@ -1,18 +1,17 @@
 #include "monty.h"
 /**
- * pall - print stack elements
+ * _pall - print stack elements
  * @stack : pointer to an address
  * @line_number : number of the line printed
  */
-void pall(stack_t **stack, unsigned int line_number)
+void _pall(stack_t **head, unsigned int line_number)
 {
     UNUSED(line_number);
-    stack_t *head;
-    head = *stack;
-
-    while (head)
+    stack_t *stack = *head;
+    
+    while (*head)
     {
-        printf("%d\n", head->n);
-        head = head->next;
+        printf("%d\n", stack->n);
+        stack = stack->next;
     }    
 }
