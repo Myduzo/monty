c33404a539f19866970031a9cd62c8edc66005ca
Myduzo <sahli.youssef@outlook.com>
2020-05-16 15:21:04 +0100

    pint_func

--- _pint.c
+++ _pint.c
@@ -1,16 +1,16 @@
 #include "monty.h"
-char *data;
 /**
- * 
- * 
+ * _pint - prints the value at the top of the stack
+ * @head : the head node
+ * @line_number : line number counter
  */
 void _pint(stack_t **head, unsigned int line_number)
 {
-    if (!*head)
-    {
-        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-        _free(*head);
-        exit(EXIT_FAILURE);
-    }
-    printf("%d\n", (*head)->n);
+	if (!*head)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		_free(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
 }
