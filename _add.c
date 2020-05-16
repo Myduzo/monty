321577c89efb8a40186a3a1703ed4507a20a33b2
Myduzo <sahli.youssef@outlook.com>
2020-05-16 21:14:28 +0100

    add function added

--- _add.c
+++ _add.c
@@ -1,25 +1,25 @@
 #include "monty.h"
 /**
- * _add - 
- * 
+ * _add - adds the top two elements of the stack
+ * @head : the head node
+ * @line_number : line number counter
  */
 void _add(stack_t **head, unsigned int line_number)
 {
-    int add;
-    stack_t *tmp;
+	int add;
+	stack_t *tmp;
 
-    tmp = (*head)->next;
+	tmp = (*head)->next;
 
-    if (!*head || !(*head)->next)
-    {
-        fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-        _free(*head);
-        exit(EXIT_FAILURE);
-    }
+	if (!*head || !(*head)->next)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		free(*head);
+		exit(EXIT_FAILURE);
+	}
 
-    add = tmp->n + (*head)->n;
-    *head = tmp;
-    (*head)->n = add;
-    (*head)->prev = NULL;
-
+	add = tmp->n + (*head)->n;
+	_pop(&(*head), line_number);
+	tmp->n = add;
+	tmp->prev = NULL;
 }
