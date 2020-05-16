a877f80ef4c0341d66e72a95af0086af23b753e4
Myduzo <sahli.youssef@outlook.com>
2020-05-17 00:53:10 +0100

    sub function added

--- _sub.c
+++ _sub.c
@@ -1,24 +1,26 @@
 #include "monty.h"
 /**
- * 
- * 
+ * _sub - subtracts the top element from the second top element of the stack
+ * @head : the head node
+ * @line_number : line number counter
  */
 void _sub(stack_t **head, unsigned int line_number)
 {
-    int sub;
-    if (!*head || !(*head)->next)
-    {
-        fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
-        _free(*head);
-        exit(EXIT_FAILURE);
-    }
+	int sub;
 
-    if ((*head)->n > (*head)->next->n)
-        sub = (*head)->n - (*head)->next->n;
-    
-    else
-        sub = (*head)->next->n - (*head)->n;
-    
-    _pop(&(*head), line_number);
-    (*head)->n = sub;
+	if (!*head || !(*head)->next)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		_free(*head);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*head)->n > (*head)->next->n)
+		sub = (*head)->n - (*head)->next->n;
+
+	else
+		sub = (*head)->next->n - (*head)->n;
+
+	_pop(&(*head), line_number);
+	(*head)->n = sub;
 }
