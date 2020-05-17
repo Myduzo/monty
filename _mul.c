f3c641b1ec4746a356263034cef54c8674ff87f3
Myduzo <sahli.youssef@outlook.com>
2020-05-17 01:52:29 +0100

    mul function added

---
+++ _mul.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * _mul - multiplies the second top element with the top element
+ * @head : the head node
+ * @line_number : line number counter
+ */
+void _mul(stack_t **head, unsigned int line_number)
+{
+	int mul;
+
+	if (!*head)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short", line_number);
+		free(*head);
+		exit(EXIT_FAILURE);
+	}
+
+	mul = (*head)->next->n * (*head)->n;
+	_pop(&(*head), line_number);
+	(*head)->n = mul;
+}
